175dfc611160d59353a56a9975d2b7ebe311b968
Randil <randiltharusha72@gmail.com>
2024-12-29 13:36:17 +0530

    Enhance CORS support in user registration API; allow credentials, support OPTIONS method, and expose Set-Cookie header

--- api/auth/user/register.php
+++ api/auth/user/register.php
@@ -4,8 +4,16 @@ require_once '../../../utils/Response.php';
 require_once '../../../utils/Database.php';
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Cookie");
+header("Access-Control-Expose-Headers: Set-Cookie");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     Response::json(['error' => 'Method not allowed'], 405);
